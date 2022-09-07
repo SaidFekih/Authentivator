3972e79262ee146eb0862a42de952c98176dffc3
Said Fekih <saidgwx@gmail.com>
2022-09-06 20:44:58 -0400

    HTML and JS first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function getDataConnexion() {
+  let email = document.getElementById("connexionEmail").value.trim();
+  let password = document.getElementById("passwordConnexion").value.trim();
+
+  if (email.length > 0 && password.length > 0) {
+    alert("Correct" + email + password);
+  } else {
+    alert("Empty");
+  }
+}
+function cancel() {
+  document.getElementById("connexionEmail").value = "";
+  document.getElementById("passwordConnexion").value = "";
+  alert("cancel");
+}
